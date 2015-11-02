df2f56d7e7ec2f4cc20251fc002016fecefb37fa
MKS-Student <mks.student@hackreactor.com>
2015-11-02 15:12:59 -0800

    getElementByClassName, stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  //need results array
+  //function within a function searching each node
+  //classList()
+  //return results array
+  var results =[];
+
+  var getNode = function(node) {
+    if(node.classList.contains(className)) {
+      results.push(node);
+    }
+    for(var i = 0; i < node.children.length; i++) {
+      getNode(node.children[i]);
+    }
+  };
+  getNode(document.body);
+
+  return results;
 };
